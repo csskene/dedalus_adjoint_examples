f88183cac85c1a04cb9f44262904638b3fe8df7c
Calum Skene <c.s.skene@leeds.ac.uk>
2024-06-11 14:12:07 +0100

    Tidy up plane Poiseuille examples. Update LBVP example to use cotangents.

--- plane_poiseuille_ivp_adj.py
+++ plane_poiseuille_ivp_adj.py
@@ -5,73 +5,57 @@ import matplotlib.pyplot as plt
 import dedalus.public as d3
 import logging
 import numpy as np
-
 from dedalus.tools import jacobi
 import scipy.sparse as sp
 import time
-
 logger = logging.getLogger(__name__)
 
 # Parameters
 Ny = 128
 dtype = np.complex128
-
 alpha = 2
 beta = 0
 Re = 3000
-
 # Bases and domain
 coords = d3.CartesianCoordinates('y')
 dist = d3.Distributor(coords, dtype=dtype)
 ybasis = d3.ChebyshevT(coords['y'], size=Ny,dealias=1, bounds=(0, 2))
-
 y, = dist.local_grids(ybasis)
-
 # Fields
 u = dist.Field(name='u', bases=(ybasis))
 v = dist.Field(name='v', bases=(ybasis))
 w = dist.Field(name='w', bases=(ybasis))
-
 p = dist.Field(name='p', bases=(ybasis))
 tau_u_1 = dist.Field(name='tau_u_1')
 tau_u_2 = dist.Field(name='tau_u_2')
-
 tau_v_1 = dist.Field(name='tau_v_1')
 tau_v_2 = dist.Field(name='tau_v_2')
-
 tau_w_1 = dist.Field(name='tau_w_1')
 tau_w_2 = dist.Field(name='tau_w_2')
-
 tau_p = dist.Field(name='tau_p')
-
 # Substitutions
 lift_basis = ybasis.derivative_basis(2)
 lift = lambda A, n: d3.Lift(A, lift_basis, n)
-
 dy = lambda A: d3.Differentiate(A, coords['y'])
-
 # Base flow
 U = dist.Field(name='U',bases=(ybasis))
 U['g'] = y*(2-y)
 Uy = dy(U)
-
 # Problem
 problem = d3.IVP([u,v,w,p, tau_u_1, tau_u_2,tau_v_1, tau_v_2,tau_w_1, tau_w_2], namespace=locals())
 problem.add_equation("dt(u) + 1j*alpha*u*U + v*Uy - 1/Re*(dy(dy(u))-alpha**2*u-beta**2*u) + lift(tau_u_1,-1) + lift(tau_u_2,-2) + 1j*alpha*p = 0")
 problem.add_equation("dt(v) + 1j*alpha*v*U - 1/Re*(dy(dy(v))-alpha**2*v-beta**2*v) + lift(tau_v_1,-1) + lift(tau_v_2,-2) + dy(p) = 0")
 problem.add_equation("dt(w) + 1j*alpha*w*U - 1/Re*(dy(dy(w))-alpha**2*w-beta**2*w) + lift(tau_w_1,-1) + lift(tau_w_2,-2) + 1j*beta*p = 0")
 problem.add_equation("1j*alpha*u + dy(v) + 1j*beta*w = 0")
-
+# Boundary conditions
 problem.add_equation("u(y=0) = 0")
 problem.add_equation("u(y=2) = 0")
-
 problem.add_equation("v(y=0) = 0")
 problem.add_equation("v(y=2) = 0")
-
 problem.add_equation("w(y=0) = 0")
 problem.add_equation("w(y=2) = 0")
-
-solver = problem.build_solver(d3.SBDF1)
+# Create solver
+solver = problem.build_solver(d3.SBDF2)
 # Get spectrally accurate weight matrices
 a_, b_ = ybasis.a, ybasis.b
 W_field = dist.Field(name='W_field', bases=(ybasis), adjoint=True)
@@ -80,7 +64,7 @@ W = W_field['g']
 # Cholesky decomposition
 M = np.sqrt(W)
 Minv = 1/M
-
+# Define direct and hermitian transpose multiplication
 def mult(vec,solver,Niter):
     # Modified state transition matrix is Phi_M = (M Phi Minv)
     # This function multiplies by Phi_M 
@@ -91,83 +75,65 @@ def mult(vec,solver,Niter):
     # reset timestepper
     solver.timestepper._iteration = 0
     solver.timestepper._LHS_params = False
-
-    vec_split = np.split(np.squeeze(vec), 3) 
-    u['g'] = Minv*vec_split[0]
-    v['g'] = Minv*vec_split[1]
-    w['g'] = Minv*vec_split[2]
-    
+    vec_split = np.split(np.squeeze(vec), 3)
+    for (i,f) in enumerate([u,v,w]):
+        f['g'] = Minv*vec_split[i]
     solver.stop_iteration = Niter
     timestep = 0.5
-    
     try:
         while solver.proceed:
             solver.step(timestep)
     except:
         logger.error('Exception raised, triggering end of main loop.')
         raise
-
-    grad = np.hstack((M*u['g'],M*v['g'],M*w['g']))
-
+    grad = np.hstack([M*f['g'] for f in [u,v,w]])
     return grad
-
 def mult_hermitian(vec,solver,Niter):
     # Modified state transition matrix is Phi_M = (M Phi Minv)
     # This function multiplies by Phi_M^H 
     solver.iteration = Niter
     solver.timestepper._LHS_params = False
-
     vec_split = np.split(np.squeeze(vec), 3)
-    solver.state_adj[0]['g'] = M*vec_split[0]
-    solver.state_adj[1]['g'] = M*vec_split[1]
-    solver.state_adj[2]['g'] = M*vec_split[2]
-    
-    timestep = 0.5
-    
+    for i in range(3):
+        solver.state_adj[i]['g'] = M*vec_split[i]
+    timestep = 0.5    
     try:
         while solver.iteration>0:
             solver.step_adjoint(timestep)
     except:
         logger.error('Exception raised, triggering end of main loop.')
         raise
-     
-    grad = np.hstack((Minv*solver.state_adj[0]['g'],Minv*solver.state_adj[1]['g'],Minv*solver.state_adj[2]['g']))
-    
+    grad = np.hstack([Minv*f['g'] for f in solver.state_adj[:3]])
     return grad
-
+# Create linear operator
 Phi = sp.linalg.LinearOperator((3*Ny,3*Ny),matvec= lambda A: mult(A,solver,100),rmatvec=lambda A: mult_hermitian(A,solver,100))
-
+# Adjoint test
 vec1 = np.random.rand(Ny*3) + 1j*np.random.rand(Ny*3)
 vec2 = np.random.rand(Ny*3) + 1j*np.random.rand(Ny*3)
-
 term2 = np.vdot(Phi.H@vec2,vec1)
 term1 = np.vdot(vec2,Phi@vec1)
-
 logger.info('Adjoint error = %g' % np.abs(term1-term2))
-
+# Loop over final times and compute transient growth
 ts_tg = time.time()
 gains = []
 times = []
 for niter in range(100)[1::5]:
-    niter=20
     Phi = sp.linalg.LinearOperator((3*Ny,3*Ny),matvec= lambda A: mult(A,solver,niter),rmatvec=lambda A: mult_hermitian(A,solver,niter))
     ts = time.time()
     UH,sigma,V = sp.linalg.svds(Phi,k=1)
     print(sigma**2)
     logger.info('T = %f, Time taken for SVD = %f s' % (niter*0.5,time.time()-ts))
-
     gains.append(sigma[-1]**2)
     times.append(niter*0.5)
-
 logger.info('Time taken for whole sweep %f' % (time.time()-ts_tg))
-
+# Plot the transient growth verus time
 fig = plt.figure(figsize=(6, 4))
 plt.plot(times,gains,'-.')
 plt.ylabel("Gain")
 plt.xlabel("T")
 plt.title("Optimal gains for plane Poiseuille flow")
 plt.savefig("plane_poiseuille_optimal_gains_transient_growth.png", dpi=200)
-
+# Plot the optimal input/output
 # plt.plot(y,Minv*UH[:Ny,-1].real)
 # plt.plot(y,Minv*V[-1,:Ny].real)
 # plt.show()
--- plane_poiseuille_lbvp_adj.py
+++ plane_poiseuille_lbvp_adj.py
@@ -5,61 +5,47 @@ import matplotlib.pyplot as plt
 import dedalus.public as d3
 import logging
 import numpy as np
-
 from dedalus.tools import jacobi
 import scipy.sparse as sp
 import time
-
 logger = logging.getLogger(__name__)
 
 # Parameters
 Ny = 128
 dtype = np.complex128
-
 alpha = -1
 beta = 0
 Re = 5000
-
 # Bases and domain
 coords = d3.CartesianCoordinates('y')
 dist = d3.Distributor(coords, dtype=dtype)
 ybasis = d3.ChebyshevT(coords['y'], size=Ny,dealias=1, bounds=(0, 2))
-
 y, = dist.local_grids(ybasis)
-
 # Fields
 u = dist.Field(name='u', bases=(ybasis))
 v = dist.Field(name='v', bases=(ybasis))
 w = dist.Field(name='w', bases=(ybasis))
-
 p = dist.Field(name='p', bases=(ybasis))
 tau_u_1 = dist.Field(name='tau_u_1')
 tau_u_2 = dist.Field(name='tau_u_2')
-
 tau_v_1 = dist.Field(name='tau_v_1')
 tau_v_2 = dist.Field(name='tau_v_2')
-
 tau_w_1 = dist.Field(name='tau_w_1')
 tau_w_2 = dist.Field(name='tau_w_2')
-
 tau_p = dist.Field(name='tau_p')
-
 # Substitutions
 lift_basis = ybasis.derivative_basis(2)
 lift = lambda A, n: d3.Lift(A, lift_basis, n)
-
 dy = lambda A: d3.Differentiate(A, coords['y'])
-
 # Base flow
 U = dist.Field(name='U',bases=(ybasis))
 U['g'] = y*(2-y)
 Uy = dy(U)
-
-# # Forcing
+# Forcing
 fu = dist.Field(name='fu',bases=(ybasis))
 fv = dist.Field(name='fv',bases=(ybasis))
 fw = dist.Field(name='fw',bases=(ybasis))
-
+# Forcing frequency
 omega = dist.Field(name='omega')
 # Problem
 problem = d3.LBVP([u,v,w,p, tau_u_1, tau_u_2,tau_v_1, tau_v_2,tau_w_1, tau_w_2], namespace=locals())
@@ -67,16 +53,14 @@ problem.add_equation("1j*omega*u + 1j*alpha*u*U + v*Uy - 1/Re*(dy(dy(u))-alpha**
 problem.add_equation("1j*omega*v + 1j*alpha*v*U - 1/Re*(dy(dy(v))-alpha**2*v-beta**2*v) + lift(tau_v_1,-1) + lift(tau_v_2,-2) + dy(p) = fv")
 problem.add_equation("1j*omega*w + 1j*alpha*w*U - 1/Re*(dy(dy(w))-alpha**2*w-beta**2*w) + lift(tau_w_1,-1) + lift(tau_w_2,-2) + 1j*beta*p = fw")
 problem.add_equation("1j*alpha*u + dy(v) + 1j*beta*w = 0")
-
+# Boundary conditions
 problem.add_equation("u(y=0) = 0")
 problem.add_equation("u(y=2) = 0")
-
 problem.add_equation("v(y=0) = 0")
 problem.add_equation("v(y=2) = 0")
-
 problem.add_equation("w(y=0) = 0")
 problem.add_equation("w(y=2) = 0")
-
+# Build solver
 solver = problem.build_solver()
 # Get spectrally accurate weight matrices
 a_, b_ = ybasis.a, ybasis.b
@@ -86,47 +70,41 @@ W = W_field['g']
 # Cholesky decomposition
 M = np.sqrt(W)
 Minv = 1/M
-
+# Create field list for adjoint RHS
+G = []
+for field in solver.state:
+    adj_field = field.copy_adjoint()
+    adj_field.preset_layout('g')
+    adj_field.data *=0
+    G.append(adj_field)
+# Define direct and hermitian transpose multiplication
 def mult(vec,solver):
     # Modified resolvent matrix is R_M = (M R Minv)
     # This function multiplies by R_M 
-
-    vec_split = np.split(np.squeeze(vec), 3) 
-    fu['g'] = Minv*vec_split[0]
-    fv['g'] = Minv*vec_split[1]
-    fw['g'] = Minv*vec_split[2]
-    
+    vec_split = np.split(np.squeeze(vec), 3)
+    for (i,f) in enumerate([fu,fv,fw]):
+        f['g'] = Minv*vec_split[i]    
     solver.solve()
-
-    grad = np.hstack((M*u['g'],M*v['g'],M*w['g']))
-
+    grad = np.hstack([M*f['g'] for f in [u,v,w]])
     return grad
-
 def mult_hermitian(vec,solver):
     # Modified state transition matrix is Phi_M = (M Phi Minv)
     # This function multiplies by Phi_M^H 
-
     vec_split = np.split(np.squeeze(vec), 3)
-    solver.dJdX[0]['g'] = M*vec_split[0]
-    solver.dJdX[1]['g'] = M*vec_split[1]
-    solver.dJdX[2]['g'] = M*vec_split[2]
-    
-    dJdf = solver.solve_adjoint([fu,fv,fw])
-
-    grad = np.hstack((Minv*dJdf[0]['g'],Minv*dJdf[1]['g'],Minv*dJdf[2]['g']))
-    
+    for i in range(3):
+        G[i]['g'] = M*vec_split[i]
+    cotangents = solver.compute_sensitivities(G)
+    grad = np.hstack([Minv*cotangents[f]['g'] for f in [fu,fv,fw]])
     return grad
-
+# Create scipy linear operator
 R = sp.linalg.LinearOperator((3*Ny,3*Ny),matvec= lambda A: mult(A,solver),rmatvec=lambda A: mult_hermitian(A,solver))
-
+# Adjoint test
 vec1 = np.random.rand(Ny*3) + 1j*np.random.rand(Ny*3)
 vec2 = np.random.rand(Ny*3) + 1j*np.random.rand(Ny*3)
-
-term2 = np.vdot(R.H@vec2,vec1)
 term1 = np.vdot(vec2,R@vec1)
-
+term2 = np.vdot(R.H@vec2,vec1)
 logger.info('Adjoint error = %g' % np.abs(term1-term2))
-
+# Loop over frequencies and compute the gain
 ts_tg = time.time()
 gains = []
 omegas = np.linspace(0.1,1.5,120)
@@ -137,11 +115,9 @@ for om in omegas:
     ts = time.time()
     UH,sigma,V = sp.linalg.svds(Phi,k=1)
     logger.info('om = %f, Time taken for SVD = %f s' % (om,time.time()-ts))
-
     gains.append(sigma[-1]**2)
-
 logger.info('Time taken for whole sweep %f' % (time.time()-ts_tg))
-
+# Plot the gain versus time
 fig = plt.figure(figsize=(6, 4))
 plt.semilogy(omegas,gains,'-.')
 plt.xlabel("Gain")
@@ -149,7 +125,7 @@ plt.ylabel("$\omega$")
 plt.title("Optimal gains for plane Poiseuille flow (forced)")
 # plt.show()
 plt.savefig("plane_poiseuille_optimal_gains_resolvent.png", dpi=200)
-
+# Plot the optimal input/output
 # plt.plot(y,Minv*UH[:Ny,-1].real)
 # plt.plot(y,Minv*V[-1,:Ny].real)
 # plt.show()
