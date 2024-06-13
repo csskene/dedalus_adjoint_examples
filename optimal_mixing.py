23e00220cccea913a5523594e0bb1c6bf147703a
Calum Skene <c.s.skene@leeds.ac.uk>
2024-06-13 15:02:14 +0100

    Initial commit of optimal mixing example.

---
+++ optimal_mixing.py
@@ -0,0 +1,228 @@
+"""
+This script solves the mixing optimisation problem presented in
+'Optimal mixing in two-dimensional stratified plane Poiseuille flow at finite Peclet and Richardson numbers'
+F. Marcotte and C.P. Caulfield - JFM 853, 359-385, 2018
+
+For now just sets up the problem and checks that the gradient passes the Taylor test
+"""
+import numpy as np
+from dedalus import public as d3
+import logging
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+# import pymanopt
+# from pymanopt.manifolds import Sphere
+# from pymanopt.optimizers import ConjugateGradient
+from scipy.special import erf
+import uuid
+
+logger = logging.getLogger(__name__)
+logging.getLogger().setLevel(logging.WARNING)
+# Parameters
+Nx = 128
+Ny = 128
+Reinv = 1./100
+Peinv = 1./100
+Ri = 0.05
+dealias = 1
+T = 0.1
+timestep = 1e-3
+timestepper = d3.SBDF2
+NIter = int(T/timestep)
+Niter = 180
+E0 = 0.02
+test = True
+
+# Domain and bases
+coords = d3.CartesianCoordinates('x','y')
+dist = d3.Distributor(coords, dtype=np.float64)
+xbasis = d3.RealFourier(coords['x'], size=Nx, bounds=(0, 2*np.pi), dealias=dealias)
+ybasis = d3.Chebyshev(coords['y'], size=Ny, bounds=(-1, 1), dealias=dealias)
+x, y = dist.local_grids(xbasis, ybasis)
+
+# Global weight matrix
+weight = 6*np.ones((Nx))*np.pi
+weight[0] *= 2
+
+# Fields and substitutions
+u  = dist.Field(name='u',bases=(xbasis,ybasis))
+v  = dist.Field(name='v',bases=(xbasis,ybasis))
+tau_u1 = dist.Field(name='tau_u1',bases=(xbasis))
+tau_u2 = dist.Field(name='tau_u2',bases=(xbasis))
+tau_v1 = dist.Field(name='tau_v1',bases=(xbasis))
+tau_v2 = dist.Field(name='tau_v2',bases=(xbasis))
+p  = dist.Field(name='p',bases=(xbasis,ybasis))
+tau_p  = dist.Field(name='tau_p')
+u0 = dist.Field(name='u0',bases=(xbasis,ybasis))
+rho  = dist.Field(name='rho',bases=(xbasis,ybasis))
+tau_rho1 = dist.Field(name='tau_rho1',bases=(xbasis))
+tau_rho2 = dist.Field(name='tau_rho2',bases=(xbasis))
+tau_rho  = dist.Field(name='tau_b',bases=(xbasis))
+cost_t  = dist.Field(name='cost_t')
+
+dx = lambda A: d3.Differentiate(A,coords['x'])
+dy = lambda A: d3.Differentiate(A,coords['y'])
+
+# Fields for the mix norm
+psi = dist.Field(name='psi',bases=(xbasis,ybasis))
+tau_psi = dist.Field(name='tau_psi') 
+tau_psi1 = dist.Field(name='tau_psi1',bases=(xbasis))
+tau_psi2 = dist.Field(name='tau_psi2',bases=(xbasis))
+
+# Lift
+lift_basis = ybasis.derivative_basis(1)
+lift = lambda A : d3.Lift(A, lift_basis, -1)
+lift_basis2 = ybasis.derivative_basis(2)
+lift2 = lambda A : d3.Lift(A, lift_basis2, -1)
+uy = dy(u) + lift(tau_u1)
+vy = dy(v) + lift(tau_v1)
+rhoy = dy(rho) + lift(tau_rho1)
+psi_y = dy(psi) + lift(tau_psi1)
+
+# Just for now
+Reinv_f = dist.Field(name='Reinv_f',bases=(xbasis,ybasis))
+Reinv_f['g'] =Reinv 
+
+# Problems
+problem = d3.IVP([u,v,rho,p,tau_u1,tau_u2,tau_v1,tau_v2,tau_rho1,tau_rho2,tau_p,cost_t], namespace=locals())
+problem.add_equation("dt(u) - Reinv*(dx(dx(u))+dy(uy)) + dx(p) + lift2(tau_u2) = Reinv_f*2 -u*dx(u) - v*dy(u)")
+problem.add_equation("dt(v) - Reinv*(dx(dx(v))+dy(vy)) + dy(p) + lift2(tau_v2) + Ri*rho = -u*dx(v) - v*dy(v)")
+problem.add_equation("dt(rho) - Peinv*(dx(dx(rho))+dy(rhoy)) + lift2(tau_rho2) = -u*dx(rho) - v*dy(rho)")
+problem.add_equation("dt(cost_t) = integ(u**2+v**2)")
+problem.add_equation("dx(u) + vy + tau_p = 0")
+problem.add_equation("integ(p) = 0")
+problem.add_equation("u(y=-1) = 0")
+problem.add_equation("u(y=1)  = 0")
+problem.add_equation("v(y=-1) = 0")
+problem.add_equation("v(y=1)  = 0")
+problem.add_equation("rhoy(y=-1) = 0")
+problem.add_equation("rhoy(y=1)  = 0")
+solver = problem.build_solver(timestepper)
+
+problem_mix_norm = d3.LBVP([psi, tau_psi1, tau_psi2, tau_psi], namespace=locals())
+problem_mix_norm.add_equation("dx(dx(psi)) + dy(psi_y) + lift(tau_psi2) + tau_psi = rho")
+problem_mix_norm.add_equation("dy(psi)(y=-1) = 0")
+problem_mix_norm.add_equation("dy(psi)(y=1) = 0")
+problem_mix_norm.add_equation("integ(psi) = 0")
+solve_psi = problem_mix_norm.build_solver()
+solve_psi.solve()
+
+# Base-flow
+u0['g'] = 1-y**2
+
+# Cost functional
+alpha = 0.5
+J = alpha/2*d3.integ(dx(psi)**2+dy(psi)**2) + (1-alpha)/(2*T)*cost_t
+Jadj = J.evaluate().copy_adjoint()
+
+# cost and gradient routines
+states_u   = []
+states_v   = []
+states_rho = []
+def forward(vec):
+    # Reset solver
+    solver.iteration = 0
+    solver.initial_iteration = 0
+    solver.evaluator.handlers[0].last_iter_div = -1
+    # reset timestepper
+    solver.timestepper._iteration = 0
+    solver.timestepper._LHS_params = False
+    solver.stop_iteration = NIter
+    vec_split = np.split(np.squeeze(vec), 2)
+    u['c'] = u0['c'] + vec_split[0].reshape((Nx,Ny))
+    v['c'] = vec_split[1].reshape((Nx,Ny))
+    rho['g'] = -0.5*erf(y/0.125)
+    cost_t['g'] = 0
+
+    states_u.clear()
+    states_v.clear()
+    states_rho.clear()
+    try:
+        while solver.proceed:
+            states_u.append(u['c'].copy())
+            states_v.append(v['c'].copy())
+            states_rho.append(rho['c'].copy())
+            solver.step(timestep)
+    except:
+        logger.error('Exception raised, triggering end of main loop.')
+        raise
+    solve_psi.solve()
+    cost = np.max(J['g'])
+    return cost
+
+# Fields for adjoint LBVP
+G = []
+for field in solve_psi.state:
+    adj_field = field.copy_adjoint()
+    adj_field.preset_layout('g')
+    adj_field.data *= 0
+    G.append(adj_field)
+
+def backward():
+    solver.iteration = NIter
+    solver.timestepper._LHS_params = False
+    # Final time condition
+    Jadj['g'] = 1
+    cotangents = {}
+    cotangents[J] = Jadj
+    id = uuid.uuid4()
+    _, cotangents =  J.evaluate_vjp(cotangents,id=id,force=True)
+    G[0]['c'] = cotangents[psi]['c']
+    cotangents_mix_norm = solve_psi.compute_sensitivities(G)
+    # Zero adjoint state
+    for f in solver.state_adj:
+        f['c'] *= 0
+    # Add contributions from cotangents (both from J, and from LBVP)
+    for (i,f) in enumerate(solver.state):
+        if f in list(cotangents_mix_norm.keys()):
+            solver.state_adj[i]['c'] += cotangents_mix_norm[f]['c']
+        if f in list(cotangents.keys()):
+            solver.state_adj[i]['c'] += cotangents[f]['c'] 
+    count = -1
+    try:
+        while solver.iteration>0:
+            u['c']   = states_u[count]
+            v['c']   = states_v[count]
+            rho['c'] = states_rho[count]
+            count -= 1
+            solver.step_adjoint(timestep)
+    except:
+        logger.error('Exception raised, triggering end of main loop.')
+        raise
+    return np.hstack((solver.state_adj[0]['c'].flatten(),solver.state_adj[1]['c'].flatten()))
+
+if test:
+    # Taylor test
+    u.fill_random('g');u['c'];u['g']
+    vecp = np.hstack((u['c'].flatten(),v['c'].flatten()))
+    u.fill_random('g');u['c'];u['g']
+    vec0 = np.hstack((u['c'].flatten(),v['c'].flatten()))
+
+    cost0 = forward(vec0)
+    grad0 = backward()
+
+    eps = 1e-3
+    costs = []
+    size = []
+    for i in range(10):
+        costp = forward(vec0+eps*vecp)
+        costs.append(costp)
+        size.append(eps)
+        eps /= 2
+
+    first = np.abs(np.array(costs)-cost0)
+    second = np.abs(np.array(costs)-cost0 - np.array(size)*np.vdot(grad0,vecp))
+
+    fig = plt.figure(figsize=(6, 4))
+    plt.loglog(size,first,label=r'First order')
+    plt.loglog(size,second,label=r'Second order')
+    plt.xlabel(r'$\epsilon$')
+    plt.ylabel(r'Error')
+    plt.title(r'Taylor test')
+    plt.legend()
+    plt.savefig('optimal_mixing_Taylor_test.png',dpi=200)
+
+    print('######## Taylor Test Results ########')
+    print('First order  : ',linregress(np.log(size), np.log(first)).slope)
+    print('Second order : ',linregress(np.log(size), np.log(second)).slope)
+    print('#####################################')
