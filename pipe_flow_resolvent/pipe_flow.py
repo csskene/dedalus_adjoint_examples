79df96b47c87094aaba0ab15f9fada587dfb65b5
Calum Skene <c.s.skene@leeds.ac.uk>
2025-03-13 16:29:22 +0000

    Update pipe flow script

--- pipe_flow_resolvent/pipe_flow.py
+++ pipe_flow_resolvent/pipe_flow.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import logging
 import read_vel
 import scipy.sparse as sp
+import dedalus.core.evaluator as evaluator
 logger = logging.getLogger(__name__)
 
 # Parameters
@@ -27,7 +28,6 @@ p = dist.Field(name='p', bases=disk)
 tau_u = dist.VectorField(coords, name='tau_u', bases=disk.edge)
 tau_w = dist.Field(name='tau_w', bases=disk.edge)
 tau_p = dist.Field(name='tau_p')
-
 f_disk = dist.VectorField(coords, name='f_disk', bases=disk)
 f_span = dist.Field(name='f_span', bases=disk)
 
@@ -50,7 +50,12 @@ Minv = 1/M
 w0 = dist.Field(name='w0', bases=disk.radial_basis)
 w0['g'] = read_vel.get_vel(r)
 
-omega = 0.6
+# Forcing frequency
+omega = dist.Field(name='omega')
+gain_1 = dist.Field(name='gain_1')
+gain_2 = dist.Field(name='gain_2')
+gain_3 = dist.Field(name='gain_3')
+
 # Problem
 problem = d3.LBVP([u, w, p, tau_u, tau_w, tau_p], namespace=locals())
 problem.add_equation("1j*omega*u + w0*dz(u) + grad(p) - (1/Re)*(lap(u)+dz(dz(u))) + lift(tau_u) = f_disk")
@@ -67,12 +72,14 @@ subproblem = solver.subproblems_by_group[(m, None)]
 u_adjoint = u.copy_adjoint()
 w_adjoint = w.copy_adjoint()
 
-# TODO: Fix this hack
+# TODO: Fix this hack (find which index is the correct m)
 layout = dist.layouts[1]
 f_span['g'] = np.exp(m*1j*phi)
 mindex = np.argmax(np.linalg.norm(f_span[layout], axis=1))
 f_span['g'] = 0
 def mult(vec):
+    u['g'] = 0
+    w['g'] = 0
     # Modified resolvent matrix is R_M = (M R Minv)
     # This function multiplies by R_M 
     vec_split = np.split(np.squeeze(vec), 3)
@@ -80,7 +87,9 @@ def mult(vec):
     f_disk[layout][1][mindex, :] = Minv*vec_split[1]
     f_span[layout][mindex, :] = Minv*vec_split[2]
     solver.solve(subproblem)
-    vec_mult = np.hstack([M*u[layout][0][mindex, :], M*u[layout][1][mindex, :], M*w[layout][mindex, :]])
+    vec_mult = np.hstack([M*u[layout][0][mindex, :], 
+                          M*u[layout][1][mindex, :], 
+                          M*w[layout][mindex, :]])
     return vec_mult
 
 def mult_hermitian(vec):
@@ -90,15 +99,18 @@ def mult_hermitian(vec):
     cotangents={}
     u_adjoint[layout][0][mindex, :] = M*vec_split[0]
     u_adjoint[layout][1][mindex, :] = M*vec_split[1]
-    w_adjoint[layout][mindex, :]    = M*vec_split[2]
+    w_adjoint[layout][mindex, :] = M*vec_split[2]
     cotangents[u] = u_adjoint
     cotangents[w] = w_adjoint
     cotangents = solver.compute_sensitivities(cotangents, subproblems=subproblem)
-    vec_mult_H = np.hstack([Minv*cotangents[f_disk][layout][0][mindex, :], Minv*cotangents[f_disk][layout][1][mindex, :], Minv*cotangents[f_span][layout][mindex, :]])
+    vec_mult_H = np.hstack([Minv*cotangents[f_disk][layout][0][mindex, :], 
+                            Minv*cotangents[f_disk][layout][1][mindex, :], 
+                            Minv*cotangents[f_span][layout][mindex, :]])
     return vec_mult_H 
 
 # Create scipy linear operator
-R = sp.linalg.LinearOperator((3*Nr, 3*Nr),matvec= lambda A: mult(A),rmatvec=lambda A: mult_hermitian(A))
+R = sp.linalg.LinearOperator((3*Nr, 3*Nr), matvec=lambda A: mult(A), 
+                             rmatvec=lambda A: mult_hermitian(A))
 # Adjoint test
 vec1 = np.random.rand(Nr*3) + 1j*np.random.rand(Nr*3)
 vec2 = np.random.rand(Nr*3) + 1j*np.random.rand(Nr*3)
@@ -106,34 +118,33 @@ term1 = np.vdot(vec2, R@vec1)
 term2 = np.vdot(R.H@vec2, vec1)
 logger.info('Adjoint error = %g' % (np.abs(term1-term2)/np.abs(term1)))
 
-# Perform the SVD
-U, sigma, VH = sp.linalg.svds(R, k=1)
-# TODO: Fix this other hack
-# Reconstruct the optimal forcing and response
-mult(np.conj(VH).T)
-
-# Plot optimal response
+# Evaluator for output
 scales = (32, 4)
-ω = d3.div(d3.skew(u)).evaluate()
-ω.change_scales(scales)
-u.change_scales(scales)
-w.change_scales(scales)
-p.change_scales(scales)
-phi, r = dist.local_grids(disk, scales=scales)
-x, y = coords.cartesian(phi, r)
+output_evaluator = evaluator.Evaluator(dist, locals())
+output_handler = output_evaluator.add_file_handler('snapshots')
+output_handler.add_task(u, scales=scales)
+output_handler.add_task(w, scales=scales)
+output_handler.add_task(f_disk, scales=scales)
+output_handler.add_task(f_span, scales=scales)
+output_handler.add_task(omega)
+output_handler.add_task(gain_1)
+output_handler.add_task(gain_2)
+output_handler.add_task(gain_3)
 
-cmap = 'RdBu_r'
-fig, ax = plt.subplots(2, 2, figsize=(6, 6))
-ax[0,0].pcolormesh(x, y, u['g'][0].real, cmap=cmap)
-ax[0,0].set_title(r"$u_\phi$")
-ax[0,1].pcolormesh(x, y, u['g'][1].real, cmap=cmap)
-ax[0,1].set_title(r"$u_r$")
-ax[1,0].pcolormesh(x, y, w['g'].real, cmap=cmap)
-ax[1,0].set_title(r"$w$")
-ax[1,1].pcolormesh(x, y, p['g'].real, cmap=cmap)
-ax[1,1].set_title(r"$p$")
-for axi in ax.flatten():
-    axi.set_aspect('equal')
-    axi.set_axis_off()
-fig.tight_layout()
-fig.savefig("pipe_output_modes.png", dpi=200)
+omegas = np.linspace(0.05, 2, 70)
+forcings = []
+for i, om in enumerate(omegas):
+    omega['g'] = om
+    solver = problem.build_solver()
+    # Perform the SVD
+    U, sigma, VH = sp.linalg.svds(R, k=3)
+    mult(np.conj(VH[-1, :]).T)
+    gain_1['g'] = sigma[-1]
+    gain_2['g'] = sigma[-2]
+    gain_3['g'] = sigma[-3]
+    output_evaluator.evaluate_handlers(output_evaluator.handlers, timestep=0, wall_time=0, sim_time=0, iteration=i)
+    u.change_scales(1)
+    w.change_scales(1)
+    f_disk.change_scales(1)
+    f_span.change_scales(1)
+    forcings.append(VH)
