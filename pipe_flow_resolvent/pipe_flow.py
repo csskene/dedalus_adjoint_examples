301569d76604ed20df613f2ce7e9e03094733359
Calum Skene <c.s.skene@leeds.ac.uk>
2025-05-14 14:31:29 +0100

    Tidy up and parallelise pipe flow example. Adjust plotting script to use contourf without gaps.

--- pipe_flow_resolvent/pipe_flow.py
+++ pipe_flow_resolvent/pipe_flow.py
@@ -1,11 +1,16 @@
+import logging
+from pathlib import Path
 import numpy as np
 import dedalus.public as d3
-import matplotlib.pyplot as plt
-import logging
 import read_vel
 import scipy.sparse as sp
+import scipy
+from mpi4py import MPI
 import dedalus.core.evaluator as evaluator
+import matplotlib.pyplot as plt
+
 logger = logging.getLogger(__name__)
+comm = MPI.COMM_WORLD
 
 # Parameters
 Re = 74345.00585608807
@@ -17,7 +22,7 @@ dtype = np.complex128
 
 # Bases
 coords = d3.PolarCoordinates('phi', 'r')
-dist = d3.Distributor(coords, dtype=dtype)
+dist = d3.Distributor(coords, dtype=dtype, comm=MPI.COMM_SELF)
 disk = d3.DiskBasis(coords, shape=(Nphi, Nr), radius=1, dtype=dtype)
 phi, r = dist.local_grids(disk)
 
@@ -42,24 +47,25 @@ dr = np.diff(r.squeeze())
 W = np.zeros(Nr)
 W[1:] = dr
 W[0] = dr[0]
-W = W*r
+W = W*np.squeeze(r)
 M = np.sqrt(W)
 Minv = 1/M
 
 # Background (Load in turbulent profile)
 w0 = dist.Field(name='w0', bases=disk.radial_basis)
-w0['g'] = read_vel.get_vel(r)
-
+profile = read_vel.get_vel(r)
+# w0['g'] = scipy.signal.savgol_filter(profile, 10, 3) # window size 10, polynomial order 3
+w0['g'] = profile
+# plt.plot(r.squeeze(), w0['g'].real.squeeze())
+# plt.show()
+# exit()
 # Forcing frequency
 omega = dist.Field(name='omega')
-gain_1 = dist.Field(name='gain_1')
-gain_2 = dist.Field(name='gain_2')
-gain_3 = dist.Field(name='gain_3')
 
 # Problem
 problem = d3.LBVP([u, w, p, tau_u, tau_w, tau_p], namespace=locals())
-problem.add_equation("1j*omega*u + w0*dz(u) + grad(p) - (1/Re)*(lap(u)+dz(dz(u))) + lift(tau_u) = f_disk")
-problem.add_equation("1j*omega*w + w0*dz(w) + u@grad(w0) + dz(p) - (1/Re)*(lap(w)+dz(dz(w))) + lift(tau_w) = f_span")
+problem.add_equation("1j*omega*u + w0*dz(u) + grad(p) - (1/Re)*(lap(u) + dz(dz(u))) + lift(tau_u) = f_disk")
+problem.add_equation("1j*omega*w + w0*dz(w) + u@grad(w0) + dz(p) - (1/Re)*(lap(w) + dz(dz(w))) + lift(tau_w) = f_span")
 problem.add_equation("div(u) + dz(w) + tau_p = 0")
 problem.add_equation("integ(p) = 0")
 problem.add_equation("u(r=1) = 0")
@@ -72,41 +78,42 @@ subproblem = solver.subproblems_by_group[(m, None)]
 u_adjoint = u.copy_adjoint()
 w_adjoint = w.copy_adjoint()
 
-# TODO: Fix this hack (find which index is the correct m)
-layout = dist.layouts[1]
-f_span['g'] = np.exp(m*1j*phi)
-mindex = np.argmax(np.linalg.norm(f_span[layout], axis=1))
-f_span['g'] = 0
-def mult(vec):
-    u['g'] = 0
-    w['g'] = 0
-    # Modified resolvent matrix is R_M = (M R Minv)
-    # This function multiplies by R_M 
+# Identify correct m indices
+weight_layout = dist.layouts[1]
+m_, _ = weight_layout.local_group_arrays(f_disk.domain, scales=1)
+m_index = m_==m
+
+# Functions to set fields
+def set_field(vec, field_disk, field_span, mat):
+    # Sets the field from vec, multiplied by mat 
     vec_split = np.split(np.squeeze(vec), 3)
-    f_disk[layout][0][mindex, :] = Minv*vec_split[0]
-    f_disk[layout][1][mindex, :] = Minv*vec_split[1]
-    f_span[layout][mindex, :] = Minv*vec_split[2]
+    field_disk[weight_layout][0, m_index] = mat*vec_split[0]
+    field_disk[weight_layout][1, m_index] = mat*vec_split[1]
+    field_span[weight_layout][m_index] = mat*vec_split[2]
+ 
+def set_vec(field_disk, field_span, mat):
+    return np.hstack([mat*field_disk[weight_layout][0, m_index], 
+                          mat*field_disk[weight_layout][1, m_index], 
+                          mat*field_span[weight_layout][m_index]])
+# Set up resolvent matrix
+def mult(vec):
+    # Modified resolvent matrix is R_M = (M R Minv)
+    # This function multiplies by R_M
+    set_field(vec, f_disk, f_span, Minv)
     solver.solve(subproblem)
-    vec_mult = np.hstack([M*u[layout][0][mindex, :], 
-                          M*u[layout][1][mindex, :], 
-                          M*w[layout][mindex, :]])
-    return vec_mult
+    matvec = set_vec(u, w, M)
+    return matvec
 
 def mult_hermitian(vec):
-    # Modified state transition matrix is Phi_M = (M Phi Minv)
-    # This function multiplies by Phi_M^H 
-    vec_split = np.split(np.squeeze(vec), 3)
-    cotangents={}
-    u_adjoint[layout][0][mindex, :] = M*vec_split[0]
-    u_adjoint[layout][1][mindex, :] = M*vec_split[1]
-    w_adjoint[layout][mindex, :] = M*vec_split[2]
+    # Modified resolvent matrix is Phi_M = (M Phi Minv)
+    # This function multiplies by Phi_M^H
+    cotangents = {}
+    set_field(vec, u_adjoint, w_adjoint, M)
     cotangents[u] = u_adjoint
     cotangents[w] = w_adjoint
     cotangents = solver.compute_sensitivities(cotangents, subproblems=subproblem)
-    vec_mult_H = np.hstack([Minv*cotangents[f_disk][layout][0][mindex, :], 
-                            Minv*cotangents[f_disk][layout][1][mindex, :], 
-                            Minv*cotangents[f_span][layout][mindex, :]])
-    return vec_mult_H 
+    rmatvec = set_vec(cotangents[f_disk], cotangents[f_span], Minv)
+    return rmatvec
 
 # Create scipy linear operator
 R = sp.linalg.LinearOperator((3*Nr, 3*Nr), matvec=lambda A: mult(A), 
@@ -118,33 +125,59 @@ term1 = np.vdot(vec2, R@vec1)
 term2 = np.vdot(R.H@vec2, vec1)
 logger.info('Adjoint error = %g' % (np.abs(term1-term2)/np.abs(term1)))
 
-# Evaluator for output
-scales = (32, 4)
-output_evaluator = evaluator.Evaluator(dist, locals())
-output_handler = output_evaluator.add_file_handler('snapshots')
-output_handler.add_task(u, scales=scales)
-output_handler.add_task(w, scales=scales)
-output_handler.add_task(f_disk, scales=scales)
-output_handler.add_task(f_span, scales=scales)
-output_handler.add_task(omega)
-output_handler.add_task(gain_1)
-output_handler.add_task(gain_2)
-output_handler.add_task(gain_3)
-
-omegas = np.linspace(0.05, 2, 70)
-forcings = []
-for i, om in enumerate(omegas):
+omega_global = np.linspace(0.05, 2, 200)
+local_gain1 = []
+local_gain2 = []
+local_gain3 = []
+omega_local = omega_global[comm.rank::comm.size]
+for om in omega_local:
     omega['g'] = om
+    # Reset scales to 1 and zero fields
+    for field in [u, w, f_disk, f_span]:
+        field.change_scales(1)
+        field['g'] = 0
+    # Rebuild solver with different omega
     solver = problem.build_solver()
     # Perform the SVD
     U, sigma, VH = sp.linalg.svds(R, k=3)
-    mult(np.conj(VH[-1, :]).T)
-    gain_1['g'] = sigma[-1]
-    gain_2['g'] = sigma[-2]
-    gain_3['g'] = sigma[-3]
-    output_evaluator.evaluate_handlers(output_evaluator.handlers, timestep=0, wall_time=0, sim_time=0, iteration=i)
-    u.change_scales(1)
-    w.change_scales(1)
-    f_disk.change_scales(1)
-    f_span.change_scales(1)
-    forcings.append(VH)
+    # Set outputs
+    # Get forcing from V
+    set_field(np.squeeze(np.conj(VH[-1, :]).T), f_disk, f_span, Minv)
+    # Get velocity from U
+    set_field(np.squeeze(U[:, -1]), u, w, Minv)
+    # Set the gains
+    local_gain1.append(sigma[-1])
+    local_gain2.append(sigma[-2])
+    local_gain3.append(sigma[-3])
+    # Save the flow data
+    scales = (10, 1)
+    output_evaluator = evaluator.Evaluator(dist, locals())
+    file_name = ('snapshots_omega_{0:.3f}'.format(om)).replace('.', '_')
+    data_dir = Path('snapshots', file_name)
+    data_dir.mkdir(parents=True, exist_ok=True)
+    output_handler = output_evaluator.add_file_handler(data_dir)
+    output_handler.add_task(u, scales=scales)
+    output_handler.add_task(w, scales=scales)
+    output_handler.add_task(f_disk, scales=scales)
+    output_handler.add_task(f_span, scales=scales)
+    output_handler.add_task(omega)
+    output_evaluator.evaluate_handlers(output_evaluator.handlers, timestep=0, wall_time=0, sim_time=0, iteration=0)
+
+# Gather outputs
+global_outputs = []
+for local_output in [local_gain1, local_gain2, local_gain3]:
+    local_output = np.array(local_output)
+    global_output = np.zeros_like(omega_global)
+    global_output[comm.rank::comm.size] = local_output
+    if comm.rank == 0:
+        comm.Reduce(MPI.IN_PLACE, global_output, op=MPI.SUM, root=0)
+    else:
+        comm.Reduce(global_output, global_output, op=MPI.SUM, root=0)
+    global_outputs.append(global_output)
+
+# Save output
+if comm.rank==0:
+    np.savez('resolvent_gains', omega=omega_global, 
+             gain_1=global_outputs[0], 
+             gain_2=global_outputs[1],
+             gain_3=global_outputs[2])
--- pipe_flow_resolvent/plot_resolvent_modes.py
+++ pipe_flow_resolvent/plot_resolvent_modes.py
@@ -10,15 +10,42 @@ from matplotlib.gridspec import GridSpec
 plt.rcParams.update({
     "text.usetex": True})
 
+def extrapolate_coordinates(phi, r):
+    """Extrapolate data to coordinate boundaries."""
+    # Repeat first phi point
+    phi_extrap = np.concatenate([phi, [2*np.pi]])
+    # Add radial endpoints
+    r_extrap = np.concatenate([[0], r, [1]])
+    return phi_extrap, r_extrap
+
+
+def extrapolate_data(phi, r, *data):
+    """Extrapolate data to coordinate boundaries."""
+    data = np.array(data)
+    # Repeat first phi point
+    _, r_extrap = extrapolate_coordinates(phi, r)
+    # Build data
+    shape = np.array(data.shape) + np.array([0, 1, 2])
+    data_extrap = np.zeros(shape=shape, dtype=data.dtype)
+    # Copy interior data
+    data_extrap[:, :-1, 1:-1] = data
+    # Copy last point in phi
+    data_extrap[:, -1, :] = data_extrap[:, 0, :]
+    # Average around origin
+    data_extrap[:, :, 0] = np.mean(data_extrap[:, :, 1], axis=(1))[:, None]
+    # Extrapolate to outer radius
+    data_extrap[:, :, -1] = data_extrap[:, :, -2] + (r_extrap[-1] - r_extrap[-2]) / (r_extrap[-2] - r_extrap[-3]) * (data_extrap[:, :, -2] - data_extrap[:, :, -3])
+    return data_extrap
+
 filename = 'snapshots/snapshots_s1.h5'
 # Plot settings
 scale = 1.5
 dpi = 300
 cm = 1/2.54  # centimeters in inches
 
-fig = plt.figure(layout="constrained", figsize=(8.7*cm, 1.3*8.7*cm))
+fig = plt.figure(figsize=(8.7*cm, 1.3*8.7*cm))
 
-gs = GridSpec(3, 3, figure=fig)
+gs = GridSpec(3, 3, figure=fig, wspace=0)
 axgain = fig.add_subplot(gs[0, :])
 ax0 = fig.add_subplot(gs[1, 0])
 ax1 = fig.add_subplot(gs[1, 1])
@@ -28,69 +55,74 @@ ax4 = fig.add_subplot(gs[2, 1])
 ax5 = fig.add_subplot(gs[2, 2])
 ax = [ax0, ax1, ax2, ax3, ax4, ax5]
 
+# Plot resolvent gains
+with np.load('resolvent_gains.npz') as file:
+    omega = file['omega']
+    axgain.semilogy(omega, file['gain_1'], 'C0', label=r'$\sigma_1$')
+    axgain.semilogy(omega, file['gain_2'], 'C1', label=r'$\sigma_2$')
+    axgain.semilogy(omega, file['gain_3'], 'C2', label=r'$\sigma_2$')
+    # Find the optimal gain
+    omega_max = omega[np.argmax(file['gain_1'])]
+axgain.set_xlabel(r'$\omega$')
+axgain.set_ylabel(r'$\sigma$')
+#axgain.grid(True)
+axgain.set_ylim([1, 4e3])
+axgain.set_xlim([0, 1.44])
+axgain.legend()
+
+# Load file corresponding to the maximum gain
+filename = ('snapshots_omega_{0:.3f}'.format(omega_max)).replace('.', '_')
+filename = 'snapshots/{0:s}/{0:s}_s1.h5'.format(filename)
 cmap = 'RdBu_r'
-# Plot writes
+# Plot optimal forcing and response
 with h5py.File(filename, mode='r') as file:
-    axgain.set_xlabel(r'$\omega$')
-    axgain.set_ylabel(r'$\sigma$')
-    #axgain.grid(True)
-    axgain.set_ylim([1, 4e3])
-    axgain.set_xlim([0, 1.44])
-    omega = file['tasks/omega']
-    gain_1 = file['tasks/gain_1']
-    gain_2 = file['tasks/gain_2']
-    gain_3 = file['tasks/gain_3']
     # Get coords
     dset_u = file['tasks/u']
     dset_w = file['tasks/w']
     dset_f_disk = file['tasks/f_disk']
     dset_f_span = file['tasks/f_span']
-    phi = dset_u.dims[2][0][:].reshape(-1, 1)
-    r = dset_u.dims[3][0][:].reshape(1, -1)
-    x = r*np.cos(phi)
-    y = r*np.sin(phi)
+    phi = dset_u.dims[2][0][:]
+    r = dset_u.dims[3][0][:]
+    phi_extrap, r_extrap = extrapolate_coordinates(phi, r)
 
-    axgain.semilogy(np.squeeze(omega), np.squeeze(gain_1), 'C0', label=r'$\sigma_1$')
-    axgain.semilogy(np.squeeze(omega), np.squeeze(gain_2), 'C1', label=r'$\sigma_2$')
-    axgain.semilogy(np.squeeze(omega), np.squeeze(gain_3), 'C2', label=r'$\sigma_2$')
-    axgain.legend()
+    phi_extrap = phi_extrap.reshape(-1, 1)
+    r_extrap = r_extrap.reshape(1, -1)
+    x = r_extrap*np.cos(phi_extrap)
+    y = r_extrap*np.sin(phi_extrap)
 
-    # Get data for the mode with the largest gain
-    index = np.argmax(np.squeeze(gain_1))
-    u = np.array(dset_u[index])
-    w = np.array(dset_w[index])
-    f_disk = np.array(dset_f_disk[index])
-    f_span = np.array(dset_f_span[index])
-
-    # Fix the phase
-    u[:] /= np.exp(1j*10*phi)
-    phase_index = np.argmax(np.abs(u[0, 0]))
-    phase_factor = u[0, 0, phase_index]
-    u[:] /= phase_factor*np.exp(-1j*10*phi)
-    w /= phase_factor
-    f_disk /= phase_factor
-    f_span /= phase_factor
+    # Load the fields
+    u_phi = np.array(dset_u[0][0])
+    u_r = np.array(dset_u[0][0])
+    u_z = np.array(dset_w[0])
+    u = extrapolate_data(r, phi, u_phi, u_r, u_z)
+    f_phi = np.array(dset_f_disk[0][0])
+    f_r = np.array(dset_f_disk[0][1])
+    f_z = np.array(dset_f_span[0])
+    f = extrapolate_data(r, phi, f_phi, f_r, f_z)
 
     # Plot the modes
     title_size = 12
-    ax[0].pcolormesh(x, y, u[0].real, cmap=cmap, rasterized=True)
-    ax[0].set_title(r"$u_\phi$", fontsize=title_size)
-    ax[1].pcolormesh(x, y, u[1].real, cmap=cmap, rasterized=True)
-    ax[1].set_title(r"$u_r$", fontsize=title_size)
-    ax[2].pcolormesh(x, y, w.real, cmap=cmap, rasterized=True)
-    ax[2].set_title(r"$u_z$", fontsize=title_size)
-    ax[3].pcolormesh(x, y, f_disk[0].real, cmap=cmap, rasterized=True)
-    ax[3].set_title(r"$f_\phi$", fontsize=title_size)
-    ax[4].pcolormesh(x, y, f_disk[1].real, cmap=cmap, rasterized=True)
-    ax[4].set_title(r"$f_r$", fontsize=title_size)
-    ax[5].pcolormesh(x, y, f_span.real, cmap=cmap, rasterized=True)
-    ax[5].set_title(r"$f_z$", fontsize=title_size)
+    flows = [u, f]
+    labels = [[r"$u_\phi$", r"$u_r$", r"$u_z$"], [r"$f_\phi$", r"$f_r$", r"$f_z$"]]
+    for i, flow in enumerate(flows):
+        for j in range(3):
+            cplt = ax[3*i + j].contourf(x, y, flow[i].real, levels=100, cmap=cmap)
+            # Rasterize
+            cplt.set_rasterized(True)
+            ax[3*i + j].set_title(labels[i][j], fontsize=title_size)
+    
+    # Add pipe outline
     for i in range(6):
         theta = np.linspace(0, 2*np.pi, 500)
         ax[i].plot(np.cos(theta), np.sin(theta), 'k-', linewidth=1)
         ax[i].set_xlim(-1.05, 1.05)
+        ax[i].set_ylim(-1.05, 1.05)
         ax[i].set_aspect('equal')
         ax[i].set_axis_off()
 
+    # Move gain axis up to prevent overlapping plots
+    pos = axgain.get_position()
+    axgain.set_position([pos.x0, pos.y0+0.1, pos.width, pos.height]) # Increase space above ax3
+
 # Save figure
-fig.savefig('pipe_flow.pdf', dpi=dpi)
+fig.savefig('pipe_flow.pdf', dpi=dpi, bbox_inches='tight')
